c00595fb57b57e564c47c5f17fbfcb454eff8c02
somar1960 <somarhh93@gmail.com>
2026-07-12 04:57:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+from instagram import check_instagram_connection
 
 app = FastAPI(
     title="SOMAR AutoSocial",
     description="AI powered Instagram automation platform"
 )
 
+
 @app.get("/")
 def home():
     return {
         "message": "SOMAR AutoSocial is running 🚀"
     }
+
+
+@app.get("/instagram-test")
+def instagram_test():
+    return check_instagram_connection()
